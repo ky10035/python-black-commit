e91ff3bc9233426e9434f4954fb99c7d51a251c6
ky10035 <ky100e5@gmail.com>
2025-01-05 00:05:06 +0900

    test: black and isort

---
+++ test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+print("this is test file.")
+ls = ["this", "is", "test", "file"]
+
+
+def PRINT():
+    test = "let's see how working this code"
+    print(test)
+
+
+PRINT()
